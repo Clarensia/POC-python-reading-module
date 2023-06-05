75aac043986beb5f03d2aa2c67c21c4a6002b4ad
Nicolas <nicolas.went@epita.fr>
2023-06-05 18:51:20 +0200

    [Main] Finished main

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from src.parser.MainClassParser import MainClassParser
 
 def main():
     parser = MainClassParser()
-    parser.parse_file("input/BlockchainAPIs")
+    main_file = parser.parse_file("input/BlockchainAPIs")
+    print(main_file)
 
 if __name__ == "__main__":
     main()
